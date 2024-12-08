16afeb382577c1d3e158050e5838151bddf65bee
Dale Hagglund <dale.hagglund@gmail.com>
2024-12-07 21:17:52 -0700

    aoc: astar search algorithm
    Extract astar search function from the puzzle .py files.
    
    By choosing `edge_cost` and `est_remaining` parameters appropriately,
    `astar` can implement any of bread-first, dijkstra's algorithm, and
    a-star.
    
    - breadth-first search: `edge_cost` and `est_remaining` return 1 and 0,
      respectively
    
    - dijkstra's algorithm: `edge_costs` returns a positive value,
      `est_remaining` returns 0.
    
    - a-star: `edge_cost` returns positive values, `est_remaining` returns
      an estimate from the current position to the target node. the
      estimated distance should underestimate the true distance in the
      graph.

---
+++ aoc/search.py
@@ -0,0 +1,79 @@
+from itertools import count
+from typing import (
+    Callable,
+    Iterator
+)
+from collections import defaultdict
+import heapq
+import functools
+
+def astar[T](
+        start: T, 
+        end: T | None, 
+        neighbours: Callable[[T], Iterator[T]], 
+        edge_cost: Callable[[T, T], int] = lambda n1, n2: 1, 
+        est_remaining: Callable[[T, T], int] = lambda n1, n2: 0,
+        verbose: int = 0,
+        statsevery: int = 500,
+    ):
+
+    seen: set[T] = set()
+    dist: defaultdict[T, float | int] = defaultdict(lambda: float('inf'))
+    prev: defaultdict[T, T | None] = defaultdict(lambda: None)
+
+    est_remaining = functools.cache(est_remaining)
+    edge_cost = functools.cache(edge_cost)
+
+    q = []
+    heapq.heapify(q)
+
+    sequence = count(1)
+    def push(item, prio):
+        heapq.heappush(q, (prio, next(sequence), item))
+    def pop():
+        _, _, item = heapq.heappop(q)
+        return item
+
+    loops, updates, rescans = 0, 0, 0
+    def display_stats():
+        print(
+            f"..."
+            f" loops {loops}"
+            f" updates {updates}"
+            f" rescans {rescans}"
+            f" assigned {len(dist)}"
+            f" seen {len(seen)}"
+        )
+
+    dist[start] = 0
+    push(start, dist[start])
+    while len(q) > 0:
+        node = pop()
+        verbose > 1 and print(f"pop: {node = }")
+        if node == end:
+            verbose > 1 and print(f"found goal: {end}")
+            break
+
+        if node in seen:
+            rescans += 1
+        
+        loops += 1
+        if verbose > 0 and loops % statsevery == 0:
+            display_stats()
+
+        for n in neighbours(node):
+            verbose > 1 and print(f"... neighbour {n}")
+            cost = edge_cost(n, node)
+            ndist = dist[node] + cost
+            verbose > 1 and print(f"... {node} -> {n}: delay {cost} newdist {ndist}")
+            if n not in dist or ndist < dist[n]:
+                if n in dist: updates += 1
+                prev[n] = node
+                dist[n] = ndist
+                push(n, dist[n] + est_remaining(n, end))
+
+        seen.add(node)
+
+    verbose > 1 and print("after search")
+    display_stats()
+    return dist, prev
