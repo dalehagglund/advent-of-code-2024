b1bd44c8ef7e1bd7d3774b225c08bc4f88e9b6b4
Dale Hagglund <dale.hagglund@gmail.com>
2024-12-08 13:55:52 -0700

    aoc.np: some numpy utilities
    extracted a few numpy related functions into a separate library.
    
    - locate(g): yields all row, column co-ordinates in numpy array `g`
      where g is nonzero. especially useful for someting like `locate(g !=
      ".")` to find all positions in the grid not equal to the "."
      character. (g is expected to be 2-dimensional.)
    
    - display(g): print a dense representation of g, which is expected to be
      a 2-d array with dtype `np.dtypes.StringDType`, whose elements are
      1-character strings.
    
    - follow_ray(g, start, dir): from (r, c) co-ordinate `start`, return the
      elements along the row, column, or diagonal given by the the direction
      `dir`. `g` is expected to be a 2-d array.

---
+++ aoc/np.py
@@ -0,0 +1,57 @@
+import numpy as np
+from typing import (
+    Iterator,
+    assert_never,
+)
+from functools import (
+    partial,
+)
+
+def locate(g) -> Iterator[tuple[int, int]]:
+    # return non-zero locations in g in a more useful form for 
+    # my python code. shoudl probably be called with an array resulting
+    # from a boolean operation of some sort.
+ 
+    s = zip(*g.nonzero())
+    s = map(partial(map, int), s)
+    s = map(tuple, s)
+    return s
+
+def display(g, prefix="    "):
+    nrow, _ = g.shape
+    for r in range(nrow):
+        print(prefix, "".join(g[r, :].flat), sep="")
+
+#
+# spent a while working this out over in the experiments directory.
+#
+def follow_ray(g, start, dir):
+    dr, dc = dir
+    nr, nc = g.shape
+    r, c = start
+
+    if (dr, dc) == (0, 0):
+        raise ValueError("direction vector is (0, 0)")
+
+    # note that `flipud(fliplr(g)) == fliplr(flipud(g))`. see below
+    # for a test confirming that.
+
+    if dc == -1:
+        # if the column direction is to the left, flip each row and
+        # flip the column index correspondingly
+        g = np.fliplr(g)
+        c = nc - 1 - c
+    if dr == -1:
+        # similarly, if the row direction is upwards, flip each column
+        # and flip the row index correspondingly
+        g = np.flipud(g)
+        r = nr - 1 - r
+
+    if 0 not in (dr, dc):
+        return np.diag(g, k=c-r)[min(r,c):]
+    elif dc == 0:
+        return g[r:, c]
+    elif dr == 0:
+        return g[r, c:]
+    else:
+        assert_never(dir)
