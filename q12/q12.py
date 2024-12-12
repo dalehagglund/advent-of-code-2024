d4bea33dd7453597a612328dd066f5c028c43ad6
Dale Hagglund <dale.hagglund@gmail.com>
2024-12-12 01:10:11 -0700

    12: broken attempt at part 2
    This solution solves the several sample inputs for part 2, *including*
    the one where the edges you have to find are direction sensitive.
    
    At the very beginning, I started down path of walking the perimeter,
    which of course turns out to be the right thing, but I thought it would
    be easier just to find horizontal and vertical sequences of edges, and
    call those sides.
    
    That works for all the sample inputs except ab.txt, because directional
    perimeter intersects there, and that breaks a vertical column into *two*
    edges, not one.
    
    I tried to hack in a (not so) quick fix for that (the cross-edge stuff)
    and that makes ab.txt work correctly, but the full input is still wrong.
    
    Sigh...

--- q12/q12.py
+++ q12/q12.py
@@ -100,11 +100,114 @@ def read_input(
         s = map(list, s)
         return np.array(list(s), dtype=np.dtypes.StringDType)
 
-def part1(filename):
+def perimeter(places, neighbours):
+    perim = 0
+    for p in places:
+        perim += 4 - sum(1 for _ in neighbours(p))
+    return perim
+
+type Edge = tuple[tuple[int, int], tuple[int, int]]
+
+def sides(places: list[tuple[int, int]], neighbours):
+    all_edges: set[Edge] = set()
+
+    def add_edge(c1, c2):
+        # print(f"... ... edge {c1 = } {c2 = }")
+        all_edges.add((c1, c2))
+
+    for r, c in places:
+        adj = set(neighbours((r, c)))
+        # print(f"... adding edges for {r, c = } {adj = }")
+        # check for non-neighbours in the top, bottom, left, right
+        # directions. any non-neighbour corresponds to an edge along
+        # that side of this cell.
+        if (r-1, c) not in adj: add_edge( (r,   c),   (r,   c+1) )
+        if (r+1, c) not in adj: add_edge( (r+1, c),   (r+1, c+1) )
+        if (r, c-1) not in adj: add_edge( (r,   c),   (r+1, c)   )
+        if (r, c+1) not in adj: add_edge( (r,   c+1), (r+1, c+1) )
+
+    # now, start pulling out arbitrary *edges* and extending af as we
+    # can go in each direction (either up/down or left/right). note
+    # that every edge is part of *exactly* one side, so there's no
+    # double counting to be worried about.
+
+    side_count = 0
+    remaining_edges = set(all_edges)
+    while remaining_edges:
+        start = remaining_edges.pop()
+
+        def follow_edge(e: Edge, dir: int) -> Iterator[Edge]:
+            c1, c2 = map(np.array, e)
+            delta = c2 - c1
+            while True:
+                if dir > 0:
+                    c1, c2 = c2, c2 + delta
+                else:
+                    c1, c2 = c1 - delta, c1
+                e = ( tuple(map(int, c1)), tuple(map(int, c2)) )
+                if e not in remaining_edges:
+                    break
+                yield e
+
+        def has_cross_edges(e1, e2) -> bool:
+            e1 = tuple(map(np.array, e1))
+            e2 = tuple(map(np.array, e2))
+            assert (e1[1] == e2[0]).all()
+
+            dir = e1[1] - e1[0]
+            if (dir == [0, 1]).all():
+                crossdir = np.array([1, 0])
+            elif (dir == [1, 0]).all():
+                crossdir = np.array([0, 1])
+            else:
+                assert False, dir
+            center = e1[1]
+
+            def to_edge(a, b):
+                return (
+                    tuple(map(int, a)),
+                    tuple(map(int, b))
+                )
+
+            return all(
+                crossedge in all_edges
+                for crossedge in [
+                    to_edge(center - crossdir, center),
+                    to_edge(center, center + crossdir)
+                ]
+            )
+
+        edges = {start}
+        for e1 in follow_edge(start, +1):
+            edges.add(e1)
+        for e1 in follow_edge(start, -1):
+            edges.add(e1)
+
+        # print(f"... found a chain: {sorted(edges)}")
+
+        final_edges = set()
+        for e1, e2 in pairwise(chain(sorted(edges), [None])):
+            if e2 is None:
+                final_edges.add(e1)
+                break
+            final_edges.add(e1)
+            if has_cross_edges(e1, e2):
+                # print(f"... cross edges at {e1, e2}")
+                break
+
+        # print(f"... final side: {sorted(final_edges)}")
+        remaining_edges -= final_edges
+        side_count += 1
+
+    # print(f"... side count: {side_count}")
+    return side_count
+
+
+def part1(filename, cost: Callable):
     grid = read_input(filename)
     rows, cols = map(range, grid.shape)
 
-    display(grid)
+    # display(grid)
 
     nodes = set(product(rows, cols))
     def neighbours(pos):
@@ -126,7 +229,7 @@ def part1(filename):
     all_plants = set(
         grid[r, c] for r, c in product(rows, cols)
     )
-    print(all_plants)
+    # print(all_plants)
 
     components = []
     for plant in all_plants:
@@ -144,16 +247,11 @@ def part1(filename):
             components.append((plant, reached))
             locs -= reached
 
-    def perimeter(places):
-        perim = 0
-        for p in places:
-            perim += 4 - sum(1 for _ in neighbours(p))
-        return perim
-
     fencing_cost = 0
     for plant, places in components:
-        print(f"{plant} {perimeter(places) = }: {places}")
-        fencing_cost += len(places) * perimeter(places)
+        cc_cost = cost(places, neighbours)
+        # print(f"{plant} cost(places, neighbours) = {cc_cost}: {places}")
+        fencing_cost += len(places) * cc_cost
     print(fencing_cost)
 
 
@@ -166,8 +264,8 @@ def usage(message):
     sys.exit(1)
 
 parts = {
-    1: part1,
-    2: part2,
+    1: partial(part1, cost=perimeter),
+    2: partial(part1, cost=sides),
 }
 
 def main(args):
