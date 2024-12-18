1aa2ae8d8bb066e6641d333dadb996e1c386dec9
Dale Hagglund <dale.hagglund@gmail.com>
2024-12-18 05:13:29 -0700

    16: solution to part 1 using new approach
    The code solves part 1 correctly again, but using a different way to
    represent the search space.
    
    - Previously, my state was a (position, dir) tuple, and the *changes*
      between the states represented moves from cell to cell or rotations.
      I'm sure this notion can be made to work, since I saw plenty of
      solutions doing so, but ultimately I found it confusing to work with.
    
    - So, I now represent what is essentially the *dual* of that approach.
      The states represent the *moves* instead of the *positions*. Each edge
      is a tuple of
    
      - a starting position + direction, and
      - an ending position + rotation
    
      This format unambiguously encodes both forward moves and right / left
      rotate moves.  Note, though, that this doesn't immediately give an easy
      way of representing *initial* positions in the graph. To deal with
      this, I allow the "starting position" to be None, which indicates an
      initial move.
    
      Each move has a cost, depending on the relationship between start and
      end. Initial moves (ie, where start is None) have a cost of zero.
    
      For an edge M1 -> M2, the cost of that edge for graph traversal is the
      cost of M2.end
    
    - So, the code here is a lot less slapdash than I was writing before. I
      guess we'll see if that makes a difference.

--- q16/q16.py
+++ q16/q16.py
@@ -69,7 +69,7 @@ from hypothesis import (
     strategies as st
 )
 
-from aoc.search import astar, allpaths, bfs
+from aoc.search import astar, dijkstra, allpaths, bfs
 from aoc.perf import timer
 from aoc.itertools import (
     star,
@@ -107,6 +107,21 @@ def innermap[T, U](
 ) -> Iterable[Iterable[U]]:
     return map(partial(map, f), s)
 
+type Dir = complex
+type Pos = complex
+
+to_dir = {
+    "N": -1 + 0j,
+    "S": +1 + 0j,
+    "E":  0 - 1j,
+    "W":  0 + 1j,
+}
+
+from_dir = dict((v, k) for k, v in to_dir.items())
+
+rot_left = 1j
+rot_right = 1j * 1j * 1j
+
 def read_input(
         filename: str
 ):
@@ -116,303 +131,105 @@ def read_input(
         s = map(list, s)
         return np.array(list(s), dtype=np.dtypes.StringDType)
 
-dirs = {
-    "^": (-1,  0),
-    ">": ( 0, +1),
-    "v": (+1,  0),
-    "<": ( 0, -1),
-}
-
-dirsym = dict((v, k) for k, v in dirs.items())
-
-class Pos(NamedTuple):
-    r: int
-    c: int
-
-    @overload
-    def __add__(self, other: tuple[int, int]) -> "Pos": ...
-    @overload
-    def __add__(self, other: "Pos") -> "Pos": ...
-
-    def __add__(self, other: "Pos") -> "Pos":
-        if isinstance(other, tuple):
-            return Pos(self.r + other[0], self.c + other[1])
-        return Pos(self.r + other.r, self.c + other.c)
-
-    @overload
-    def __sub__(self, other: tuple[int, int]) -> "Pos": ...
-    @overload
-    def __sub__(self, other: "Pos") -> "Pos": ...
-    def __sub__(self, other: "Pos") -> "Pos":
-        if isinstance(other, tuple):
-            return Pos(self.r - other[0], self.c - other[1])
-        return Pos(self.r - other.r, self.c - other.c)
-
-
 @dataclass(frozen=True)
-class State:
-    pos: Pos
-    dir: tuple[int, int]
+class Move:
+    from_: tuple[Pos, Dir] | None
+    to_: tuple[Pos, Dir]
+
+    def __post_init__(self):
+        if self.from_ is not None:
+            assert self.from_[1] in to_dir.values(), self
+        assert self.to_[1] in to_dir.values(), self
+        if self.from_ is None:
+            return
+
+        fpos, fdir = self.from_
+        tpos, tdir = self.to_
+
+        if fpos == tpos:
+            assert tdir in (fdir * rot_left, fdir * rot_right), self
+        elif fpos + fdir == tpos:
+            assert fdir == tdir, self
+        else:
+            assert False, self
+
     def __repr__(self):
-        r, c = self.pos
-        s = dirsym[self.dir]
-        return f"({r}, {c}, {s})"
+        def fmt(end: tuple[Pos, Dir] | None) -> str:
+            if end is None: return "None"
+            pos, dir = end
+            r, c = map(int, (pos.real, pos.imag))
+            return f"{from_dir[dir]}@{r, c}"
+        return f"{fmt(self.from_)} -> {fmt(self.to_)}"
 
+    def cost(self) -> int:
+        if self.from_ is None:
+            # a magic *initial* move to bring the reindeer onto the
+            # board
+            return 0
 
+        fpos, fdir = self.from_
+        tpos, tdir = self.to_
 
+        if fpos == tpos:
+            return 1000
 
-def part1(filename, part2=False):
-    grid = read_input(filename)
-    # display(grid)
-    nrow, ncol = grid.shape
-
-    board: defaultdict[Pos, set[tuple[int, int]]] = defaultdict(set)
-    for pos in map(star(Pos), product(range(1, nrow-1), range(1, ncol - 1))):
-        for _, dir in dirs.items():
-            if grid[pos + dir] == "#":
-                continue
-            board[pos].add(dir)
-
-    # print(sorted(board.items(), key=star(lambda k, v: k)))
-
-    start = Pos(*only(locate(grid == "S")))
-    end = Pos(*only(locate(grid == "E")))
-
-    print(f"{start, end = }")
-
-    def edge_cost(u: State, v: State) -> int:
-        if u.pos != v.pos:
-            assert u.dir == v.dir
-            assert u.pos + u.dir == v.pos
+        if fpos + fdir == tpos:
             return 1
-        dotprod = sum(map(operator.mul, u.dir, v.dir))
-        match dotprod:
-            # case  1: return 1
-            case  0: return 1000
-            case -1: return 2000
-            case _:
-                assert_never(dotprod)
 
-    def moves(u: State) -> Iterator[State]:
-        if u.dir in board[u.pos]:
-            # move forward in the current direction
-            yield State(u.pos + u.dir, u.dir)
-        for dir in board[u.pos] - {u.dir}:
-            # turn in place
-            yield State(u.pos, dir)
+        assert False, ("impossible move state!", self)
 
-    def extract_path(prev, s) -> Iterable[State]:
-        path = [s]
-        while prev[s] is not None:
-            path.append(prev[s])
-            s = prev[s]
-        path.reverse()
-        return path
+def part1(filename):
+    grid = read_input(filename)
 
-    def distance(u: State, _) -> int:
-        return abs(u.pos.r - end.r) + abs(u.pos.c - end.c)
+    rows, cols = map(range, grid.shape)
+    start: tuple[int, int] = only(locate(grid == "S"))
+    end: tuple[int, int] = only(locate(grid == "E"))
 
-    def is_goal(target: Pos, u: State) -> bool:
-        return u.pos == target
+    def inbounds(pos: Pos) -> bool:
+        return pos.real in rows and pos.imag in cols
+    def at(pos: Pos) -> str:
+        r, c = map(int, (pos.real, pos.imag))
+        return grid[r, c]
+    def to_pos(p: tuple[int, int]) -> complex:
+        r, c = p
+        return r + c*1j
+    def to_pair(p: Pos) -> tuple[int, int]:
+        return int(p.real), int(p.imag)
 
-    from_start, prev_from_end = astar(
-        State(pos=start, dir=dirs["<"]),
-        None,
-        moves,
-        edge_cost=edge_cost,
-        # est_remaining=distance
+    move_graph: defaultdict[tuple[Pos, Dir], set[Move]] = defaultdict(set)
+    for r, c, dir in product(rows, cols, to_dir.values()):
+        pos = to_pos((r, c))
+        if not inbounds(pos) or at(pos) == "#":
+            continue
+
+        # add a forward move
+        if inbounds(pos + dir) and at(pos + dir) != "#":
+            move_graph[(pos, dir)].add(
+                Move((pos, dir), (pos + dir, dir))
+            )
+        # add rotational moves
+        for newdir in [dir * rot_left, dir * rot_right]:
+            move_graph[(pos, dir)].add(
+                Move((pos, dir), (pos, newdir))
+            )
+
+    starting_move = Move(None, (to_pos(start), to_dir["E"]))
+
+    ending_moves = set(
+        Move((to_pos(end) + dir, -dir), (to_pos(end), -dir))
+        for dir in map(lambda d: to_dir[d], "NSEW")
+        if inbounds(to_pos(end) + dir) and at(to_pos(end) + dir) != "#"
     )
 
-    min_state = min(
-        filter(lambda s: s.pos == end, from_start.keys()),
-        key=lambda s: from_start[s]
+    dist, _ = astar(
+        starting_move,
+        None and (lambda e: e in ending_moves),
+        functools.cache(lambda e: move_graph[e.to_]),
+        #next_moves,
+        edge_cost = lambda m1, m2: m2.cost()
     )
-    min_cost = from_start[min_state]
-    print(f"{part2, min_state, from_start[min_state] = }")
-
-    # pathgrid = grid.copy()
-    # for s in best_seats:
-    #     pathgrid[s.pos] = "@"
-    # display(pathgrid)
-
-    # print(len(best_seats))
-
-    if not part2:
-        return
-
-    # def all_shortest_paths(
-    #         path: list[State],
-    #         visited: set[State],
-    #         cost: int,
-    #         depth: int = 0,
-    #         trace: bool = False
-    # ) -> Iterable[list[State]]:
-    #     pref = " " * depth
-    #     v = path[-1]
-    #     vprev = prev_from_end[v]
-    #     assert isinstance(vprev, State)
-    #     if trace: print(
-    #         f"{pref}> {cost = } {v = !s} {vprev = !s}"
-    #     )
-    #     if vprev is None:
-    #         assert path[-1].pos == start
-    #         assert path[0].pos == end
-    #         for s, t in pairwise(reversed(path)):
-    #             assert t in moves(v)
-    #         if trace: print(
-    #             f"{pref}> yield {path}"
-    #         )
-    #         yield path.copy()
-    #         return
-
-    #     assert v in set(moves(vprev))
-    #     for u in backward_moves(v):
-    #         if trace: print(
-    #             f"{pref}>"
-    #             f" backward move {u!s}"
-    #         )
-    #         if v not in set(moves(u)):
-    #             continue
-    #         uvcost = edge_cost(u, v)
-    #         if from_start[u] + uvcost != cost:
-    #             continue
-    #         path.append(u)
-    #         visited.add(u)
-    #         yield from all_shortest_paths(
-    #             path, visited, cost - uvcost, depth+1, trace=trace
-    #         )
-    #         visited.remove(u)
-    #         path.pop()
-
-    print("!!! HELLO!!!")
-
-    def all_best_paths(
-            path: list[State],
-            trace: bool=False,
-            depth: int=0,
-            trlim: int=1000000000
-    ) -> Iterator[list[State]]:
-        def out(*args):
-            if trace and depth <= trlim:
-                print(f'{" "*depth}', *args)
-        def tr(*args): out("...", *args)
-        def enter(*args): out(">", *args)
-        def exit(*args): out("<", *args)
-
-        v = path[-1]
-        vprev = prev_from_end[v]
-        vcost = from_start[v]
-        enter(f"{v = } {vcost = } {vprev = }")
-        if vprev is None:
-            tr(f"found {path = }")
-            yield path[::-1]
-
-        recur = partial(
-            all_best_paths,
-            trace=trace,
-            depth=depth+1,
-            trlim=trlim
-        )
-
-        # we can end up at v from a previous state u via one of
-        # several possible turns in place, or a move a neighbouring
-        # node *in the v.dir* direction. so, compute the candidate
-        # predecessors. some of these might not be valid, but we'll
-        # discard those later.
-
-        ucand = set()
-        # back up a potential forward move
-        if v.pos - v.dir in board:
-            ucand.add(State(v.pos - v.dir, v.dir))
-        # for each *other* direction at this position, we could have
-        # rotated to our current direction
-        for dir in set(dirs.values()) - {v.dir}:
-            ucand.add(State(v.pos, dir))
-
-        tr(f"vprev {vprev} ucand {list(ucand)}")
-        for u in ucand:
-            uvcost = edge_cost(u, v)
-            if from_start[u] + uvcost != vcost:
-                tr(f"{u, v = }: costs don't work: {from_start[u], uvcost, vcost = }")
-                # u can't be on a best path to v
-                continue
-            path.append(u)
-            yield from recur(path)
-            path.pop()
-
-        exit()
-
-
-    tiles = set()
-    for path in all_best_paths([min_state]):
-        assert path[0].pos == start
-        assert path[-1].pos == end
-        for u, v in pairwise(path):
-            assert v in moves(u)
-        assert min_cost == sum(
-            edge_cost(u, v) for u, v in pairwise(path)
-        )
-        for u, v in pairwise(path):
-            tiles.add(u.pos)
-            tiles.add(v.pos)
-
-    print(f"{tiles = }")
-    pathgrid = grid.copy()
-    if len(tiles) > 0: pathgrid[*np.transpose(list(tiles))] = "O"
-    display(pathgrid)
-    print(len(tiles))
-    return
-
-    from_end, _ = astar(
-        { State(pos=end, dir=dir) for dir in dirs.values() },
-        partial(is_goal, start),
-        moves,
-        edge_cost=edge_cost,
-    )
-
-    flip = dict(batched("<>><^vv^", n=2))
-    tiles = set()
-    for pos in map(star(Pos), product(range(1,nrow-1), range(1,ncol-1))):
-        for dir in "^>v<":
-            sdir = State(pos, dirs[dir])
-            sflip = State(pos, dirs[flip[dir]])
-
-            if sdir not in from_start or sflip not in from_end:
-                if pos == (13, 13):
-                    print(f"... {pos, dir, flip[dir] = }")
-                    print("... ... from_start", list(filter(lambda s: s.pos == pos, from_start.keys())))
-                    print("... ... from_end", list(filter(lambda s: s.pos == pos, from_end.keys())))
-                continue
-            if from_start[sdir] + from_end[sflip] == min_cost:
-                print(f"... {pos = }")
-                tiles.add(pos)
-    print(f"{len(tiles) = }")
-
-    print(nrow, ncol)
-    pathgrid = grid.copy()
-    pathgrid[*np.transpose(list(tiles))] = "O"
-    display(pathgrid)
-
-    tiles = set()
-    # for i, path in enumerate(allpaths(
-    #     [ State(pos=start, dir=directions["<"]) ],
-    #     { State(pos=start, dir=directions["<"]) },
-    #     moves,
-    #     # keep = lambda p: p[-1].pos == end,
-    # )):
-    #     if i % 5000== 0: print(f"... {i}")
-    #     if path[-1].pos == end:
-    #         continue
-    #     if dist[path[-1]] != best_cost:
-    #         continue
-
-    #     state = path[-1]
-    #     tiles.add(state.pos)
-    #     while prev[state] is not None:
-    #         tiles.add(prev[state].pos)
-    #         state = prev[state]
-
-    print(len(tiles))
+    for e in ending_moves:
+        print(f"... dist[{e}] = {dist[e]}")
 
 def part2(filename):
     ...
@@ -424,7 +241,7 @@ def usage(message):
 
 parts = {
     1: part1,
-    2: partial(part1, part2=True),
+    2: part2,
 }
 
 def main(args):
