ee5ee061d6447b090990b9398a8163110d8578a7
Dale Hagglund <dale.hagglund@gmail.com>
2024-12-19 22:43:35 -0700

    20: part 1 solution

--- q20/q20.py
+++ q20/q20.py
@@ -109,13 +109,91 @@ def innermap[T, U](
 
 def read_input(
         filename: str
-):
+) -> np.ndarray:
     with open(filename) as f:
         s = iter(f)
-        ...
+        s = map(str.rstrip, s)
+        s = map(list, s)
+        return np.array(
+            list(s),
+            dtype=np.dtypes.StringDType
+        )
+
+all_dirs = [
+    (-1,  0),
+    (+1,  0),
+    ( 0, -1),
+    ( 0, +1),
+]
 
 def part1(filename):
-    ...
+    grid = read_input(filename)
+    rows, cols = map(range, grid.shape)
+
+    positions = set(product(rows, cols))
+
+    def neighbours(pos) -> Iterator[tuple[int, int]]:
+        r, c = pos
+        for dr, dc in all_dirs:
+            if (r + dr, c + dc) not in positions:
+                continue
+            if grid[r + dr, c + dc] == "#":
+                continue
+            yield r + dr, c + dc
+
+    def extract_path(prev, n) -> list:
+        path = [n]
+        while (n := prev[n]) is not None:
+            path.append(n)
+        path.reverse()
+        return path
+
+    start = only(locate(grid == "S"))
+    end = only(locate(grid == "E"))
+    dist, prev = astar(
+        start,
+        None,
+        neighbours,
+    )
+    print(f"{end = } {dist[end] = }")
+    shortest_path = extract_path(prev, end)
+    nodes_on_path = set(shortest_path)
+    # print(f"{shortest_path = }")
+
+    def try_cheat(pos, dir) -> int:
+        (r, c), (dr, dc) = pos, dir
+        assert pos in nodes_on_path
+
+        if (r + dr, c + dc) not in positions:
+            return 0
+        if grid[r + dr, c + dc] != "#":
+            return 0
+        if (r + 2 * dr, c + 2 * dc) not in positions:
+            return 0
+        if (ch := grid[r + 2 * dr, c + 2 * dc]) == "#":
+            return 0
+        assert ch in ("S", "E", ".")
+
+        posdist = dist[r, c]
+        cheatdist = 2 + dist[r + 2 * dr, c + 2 * dc]
+
+        if cheatdist > posdist:
+            return 0
+        return posdist - cheatdist
+
+    savings: defaultdict[int, set] = defaultdict(set)
+    for pos, dir in product(shortest_path, all_dirs):
+        saved = try_cheat(pos, dir)
+        if saved > 0:
+            savings[saved].add((pos, dir))
+
+    for k, v in sorted(savings.items(), key=star(lambda k, _: k)):
+        print(f"{k} picoseconds: {len(v)} cheats")
+
+    above_100 = 0
+    for saved, cheats in savings.items():
+        if saved >= 100: above_100 += len(cheats)
+    print(f"{above_100 = }")
 
 def part2(filename):
     ...
